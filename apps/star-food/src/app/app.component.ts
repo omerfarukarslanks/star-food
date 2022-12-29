628a68ae8055f284679f8690c1f0d921afc0dffd
omerfrkarslan <omer.arslan@net-bt.com.tr>
2022-12-29 16:03:59 +0300

    Initial commit

---
+++ apps/star-food/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'star-food-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'star-food';
+}
